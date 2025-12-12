06051f1b119e96bc7f9868be860e6bae0be18a45
tarun-pratap <7tarun7singh@gmail.com>
2025-12-12 20:25:03 +0530

    Initial Commit

---
+++ Server/src/index.ts
@@ -0,0 +1,43 @@
+import express, { Request, Response } from "express";
+import http, { Server } from "http";
+import WebSocket from "ws";
+
+const app = express();
+const server = http.createServer(app);
+
+// Normal HTTP route
+app.get("/", (req, res) => {
+    res.send("This is a normal HTTPS/HTTP page.");
+});
+
+//Create WS server
+const wss = new WebSocket.Server({ noServer: true });
+console.log("🚀 ~ wss:", wss)
+
+//Handle WebSocket connection
+wss.on("connection", (ws) => {
+    ws.on("message", (msg) => {
+        console.log("VPN traffic received:", msg.toString());
+
+        //send wrapped VPN response
+        ws.send("Received: " + msg);
+    });
+});
+
+//Upgrad HTTP -> WebSocket
+server.on("upgrade",(req,socket,head)=>{
+    console.log(head,"head")
+    if(req.url==="/ws"){
+        console.log("inside");
+        wss.handleUpgrade(req,socket,head,(ws)=>{
+            // console.log(ws);
+            wss.emit("connection",ws,req);
+        });
+    }else {
+        socket.destroy();
+    }
+});
+
+server.listen(8080, () => console.log("Server runnning on 8080"));
+
+// NDKDE+zJvpnPHtVBmQ0Qzw==
